44616f7def968aa3c32f7fd6403d50e9912b5053
Ryan Boone <falldowngoboone@gmail.com>
2016-01-07 14:08:00 -0600

    Completed Chapter Five. Data is now peristent via Local Storage and Alt

---
+++ app/libs/alt.js
@@ -0,0 +1,11 @@
+import Alt from 'alt';
+//import chromeDebug from 'alt-utils/lib/chromeDebug';
+
+const alt = new Alt();
+//chromeDebug(alt);
+
+export default alt;
+// There is a Chrome plugin known as alt-devtool<https://github.com/goatslacker/alt-devtool>. 
+// After it is installed, you can connect to Alt by uncommenting the related 
+// lines above. You can use it to debug the state of your stores, search, and 
+// travel in time.
